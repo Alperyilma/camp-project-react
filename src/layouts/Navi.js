12a054e191ece48d6aa543afbd303c49e05e6a46
Alperyilma <alperyilmaz.be@gmail.com>
2022-02-10 01:14:37 +0100

    10 Feb 0114

--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,47 +1,42 @@
-import React, {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import {Container, Menu } from 'semantic-ui-react'
-import CartSummary from './CartSummary';
-import SignedIn from './SignedIn';
-import SignedOut from './SignedOut';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Menu } from "semantic-ui-react";
+import CartSummary from "./CartSummary";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
+import { useSelector } from "react-redux";
 
 export default function Navi() {
-
+  const { cartItems } = useSelector((state) => state.cart);
   const [isAuthenticated, setIsAuthenticated] = useState(true);
-  const history = useNavigate()
+  const history = useNavigate();
 
-
-  function handleSignOut(){
-    setIsAuthenticated(false)
-    history("/")
+  function handleSignOut() {
+    setIsAuthenticated(false);
+    history("/");
   }
 
   function handleSignIn(params) {
-    setIsAuthenticated(true)
+    setIsAuthenticated(true);
   }
 
-  return <div>
-    <Menu inverted fixed="top">
-      <Container>
+  return (
+    <div>
+      <Menu inverted fixed="top">
+        <Container>
+          <Menu.Item name="home" />
+          <Menu.Item name="messages" />
 
-      <Menu.Item
-          name='home'
-        />
-        <Menu.Item
-          name='messages'
-        />
-
-        <Menu.Menu position='right'>
-          <CartSummary/>
-          {
-            isAuthenticated ?<SignedIn singOut={handleSignOut}/>:<SignedOut signIn={handleSignIn}/>
-          }
-
-          
-        </Menu.Menu>
-      </Container>
-    
-       
+          <Menu.Menu position="right">
+            {cartItems.length > 0 && <CartSummary />}
+            {isAuthenticated ? (
+              <SignedIn singOut={handleSignOut} />
+            ) : (
+              <SignedOut signIn={handleSignIn} />
+            )}
+          </Menu.Menu>
+        </Container>
       </Menu>
-  </div>;
+    </div>
+  );
 }
